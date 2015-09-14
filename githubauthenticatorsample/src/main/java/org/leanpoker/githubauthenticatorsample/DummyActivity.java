86abcc7d78039b46ae3f6ad04b930addc53f733e
Tibor Molnar <fatalaa@hotmail.com>
2015-09-14 22:10:18 +0200

    Github Auth sample tweaks

---
+++ githubauthenticatorsample/src/main/java/org/leanpoker/githubauthenticatorsample/DummyActivity.java
@@ -0,0 +1,15 @@
+package org.leanpoker.githubauthenticatorsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tmolnar on 14/09/15.
+ */
+public class DummyActivity extends Activity {
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy);
+    }
+}
--- githubauthenticatorsample/src/main/java/org/leanpoker/githubauthenticatorsample/MainActivity.java
+++ githubauthenticatorsample/src/main/java/org/leanpoker/githubauthenticatorsample/MainActivity.java
@@ -1,31 +1,67 @@
 package org.leanpoker.githubauthenticatorsample;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
+import com.airbnb.deeplinkdispatch.DeepLink;
+
+import java.util.UUID;
+
+//@DeepLink("org.leanpoker.githubauthenticatorsample://github-login")
 public class MainActivity extends AppCompatActivity {
 
+    public static String OAUTH_URL = "https://github.com/login/oauth/authorize";
+    public static String OAUTH_ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token";
+
+    public static String CLIENT_ID = "3826c0b84ae756bb1e5f";
+    public static String CLIENT_SECRET = "24d2417f9c803bb03fb20190ca439d92d1a5f6d3";
+    public static String CALLBACK_URL = "http://localhost";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        String url = OAUTH_URL + "?client_id=" + CLIENT_ID + "&callback_uri=leanpoker://github-login";
+
+        WebView webview = (WebView)findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient() {
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                String accessTokenFragment = "access_token=";
+                String accessCodeFragment = "code=";
+
+                // We hijack the GET request to extract the OAuth parameters
+
+                if (url.contains(accessTokenFragment)) {
+                    // the GET request contains directly the token
+                    String accessToken = url.substring(url.indexOf(accessTokenFragment));
+                    TokenStorer.setAccessToken(accessToken);
+
+
+                } else if (url.contains(accessCodeFragment)) {
+                    Log.d("ASD", url);
+                    // the GET request contains an authorization code
+                    String accessCode = url.substring(url.indexOf(accessCodeFragment));
+                    TokenStorer.setAccessCode(accessCode);
+
+
+                    String query = "client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=" + accessCode;
+                    view.postUrl(OAUTH_ACCESS_TOKEN_URL, query.getBytes());
+                }
             }
         });
+        webview.loadUrl(url);
+        Log.d("ASD", url);
     }
 
     @Override
@@ -48,5 +84,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, "Deep Link: " + message, Toast.LENGTH_SHORT).show();
     }
 }
