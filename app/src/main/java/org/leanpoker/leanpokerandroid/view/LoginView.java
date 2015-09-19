1d1479fe3e1062929423ef892672b045c20b3f73
Tibor Molnar <fatalaa@hotmail.com>
2015-09-19 16:21:14 +0200

    TokenStore uses encrypted storage

--- app/src/main/java/org/leanpoker/leanpokerandroid/view/LoginView.java
+++ app/src/main/java/org/leanpoker/leanpokerandroid/view/LoginView.java
@@ -1,8 +1,11 @@
 package org.leanpoker.leanpokerandroid.view;
 
+import android.content.Context;
+
 /**
  * Created by tmolnar on 19/09/15.
  */
 public interface LoginView {
     void navigateToEventsAcitivity();
+    Context getContext();
 }
--- data/src/main/java/org/leanpoker/data/store/TokenStore.java
+++ data/src/main/java/org/leanpoker/data/store/TokenStore.java
@@ -1,5 +1,11 @@
 package org.leanpoker.data.store;
 
+import android.content.Context;
+
+import com.orhanobut.hawk.Hawk;
+import com.orhanobut.hawk.HawkBuilder;
+import com.orhanobut.hawk.LogLevel;
+
 import org.leanpoker.data.model.AccessToken;
 
 /**
@@ -7,21 +13,37 @@ import org.leanpoker.data.model.AccessToken;
  */
 public class TokenStore {
 
-    private static TokenStore mInstance = new TokenStore();
+    private static TokenStore mInstance;
+    public static final String TOKEN_STORAGE_KEY = "gh_access_token";
 
     private AccessToken mAccessToken;
 
-    private TokenStore() {}
+    private TokenStore(Context context) {
+        Hawk.init(context)
+                .setEncryptionMethod(HawkBuilder.EncryptionMethod.HIGHEST)
+                .setLogLevel(LogLevel.FULL)
+                .setStorage(HawkBuilder.newSharedPrefStorage(context))
+                .build();
+    }
 
-    public static TokenStore getInstance() {
+    public static TokenStore getInstance(Context context) {
+        if (mInstance == null) {
+            synchronized (TokenStore.class) {
+                if (mInstance == null) {
+                    mInstance = new TokenStore(context);
+                }
+            }
+        }
         return mInstance;
     }
 
     public AccessToken getAccessToken() {
+        mAccessToken = Hawk.get(TOKEN_STORAGE_KEY);
         return mAccessToken;
     }
 
     public void setAccessToken(AccessToken accessToken) {
+        Hawk.put(TOKEN_STORAGE_KEY, accessToken);
         mAccessToken = accessToken;
     }
 }
